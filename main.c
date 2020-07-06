da85ecc248fbe6750268bc5b59700e97fa6b2485
Vincent Lemesle <vincent.lemesle@epitech.com>
2020-07-06 15:29:51 +0200

    main.c

--- main.c
+++ main.c
@@ -5,136 +5,171 @@
   See the file COPYING.
 */
 #define FUSE_USE_VERSION 31
-#include <fuse.h>
+#include <fuse_lowlevel.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <stddef.h>
+#include <unistd.h>
 #include <assert.h>
-/*
- * Command line options
- *
- * We can't set default values for the char* fields here because
- * fuse_opt_parse would attempt to free() them when the user specifies
- * different values on the command line.
- */
-static struct options {
-    const char *filename;
-    const char *contents;
-    int show_help;
-} options;
-#define OPTION(t, p)                           \
-    { t, offsetof(struct options, p), 1 }
-static const struct fuse_opt option_spec[] = {
-        OPTION("--name=%s", filename),
-        OPTION("--contents=%s", contents),
-        OPTION("-h", show_help),
-        OPTION("--help", show_help),
-        FUSE_OPT_END
-};
-static void *hello_init(struct fuse_conn_info *conn,
-                        struct fuse_config *cfg)
+static const char *hello_str = "Hello World!\n";
+static const char *hello_name = "hello";
+static int hello_stat(fuse_ino_t ino, struct stat *stbuf)
 {
-    (void) conn;
-    cfg->kernel_cache = 1;
-    return NULL;
-}
-static int hello_getattr(const char *path, struct stat *stbuf,
-                         struct fuse_file_info *fi)
-{
-    (void) fi;
-    int res = 0;
-    memset(stbuf, 0, sizeof(struct stat));
-    if (strcmp(path, "/") == 0) {
-        stbuf->st_mode = S_IFDIR | 0755;
-        stbuf->st_nlink = 2;
-    } else if (strcmp(path+1, options.filename) == 0) {
-        stbuf->st_mode = S_IFREG | 0444;
-        stbuf->st_nlink = 1;
-        stbuf->st_size = strlen(options.contents);
-    } else
-        res = -ENOENT;
-    return res;
-}
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                         off_t offset, struct fuse_file_info *fi,
-                         enum fuse_readdir_flags flags)
-{
-    (void) offset;
-    (void) fi;
-    (void) flags;
-    if (strcmp(path, "/") != 0)
-        return -ENOENT;
-    filler(buf, ".", NULL, 0, 0);
-    filler(buf, "..", NULL, 0, 0);
-    filler(buf, options.filename, NULL, 0, 0);
+    stbuf->st_ino = ino;
+    switch (ino) {
+        case 1:
+            stbuf->st_mode = S_IFDIR | 0755;
+            stbuf->st_nlink = 2;
+            break;
+        case 2:
+            stbuf->st_mode = S_IFREG | 0444;
+            stbuf->st_nlink = 1;
+            stbuf->st_size = strlen(hello_str);
+            break;
+        default:
+            return -1;
+    }
     return 0;
 }
-static int hello_open(const char *path, struct fuse_file_info *fi)
+static void hello_ll_getattr(fuse_req_t req, fuse_ino_t ino,
+                             struct fuse_file_info *fi)
 {
-    if (strcmp(path+1, options.filename) != 0)
-        return -ENOENT;
-    if ((fi->flags & O_ACCMODE) != O_RDONLY)
-        return -EACCES;
-    return 0;
-}
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
-                      struct fuse_file_info *fi)
-{
-    size_t len;
+    struct stat stbuf;
     (void) fi;
-    if(strcmp(path+1, options.filename) != 0)
-        return -ENOENT;
-    len = strlen(options.contents);
-    if (offset < len) {
-        if (offset + size > len)
-            size = len - offset;
-        memcpy(buf, options.contents + offset, size);
-    } else
-        size = 0;
-    return size;
+    memset(&stbuf, 0, sizeof(stbuf));
+    if (hello_stat(ino, &stbuf) == -1)
+        fuse_reply_err(req, ENOENT);
+    else
+        fuse_reply_attr(req, &stbuf, 1.0);
 }
-static const struct fuse_operations hello_oper = {
-        .init           = hello_init,
-        .getattr        = hello_getattr,
-        .readdir        = hello_readdir,
-        .open           = hello_open,
-        .read           = hello_read,
-};
-static void show_help(const char *progname)
+static void hello_ll_lookup(fuse_req_t req, fuse_ino_t parent, const char *name)
 {
-    printf("usage: %s [options] <mountpoint>\n\n", progname);
-    printf("File-system specific options:\n"
-           "    --name=<s>          Name of the \"hello\" file\n"
-           "                        (default: \"hello\")\n"
-           "    --contents=<s>      Contents \"hello\" file\n"
-           "                        (default \"Hello, World!\\n\")\n"
-           "\n");
+    struct fuse_entry_param e;
+    if (parent != 1 || strcmp(name, hello_name) != 0)
+        fuse_reply_err(req, ENOENT);
+    else {
+        memset(&e, 0, sizeof(e));
+        e.ino = 2;
+        e.attr_timeout = 1.0;
+        e.entry_timeout = 1.0;
+        hello_stat(e.ino, &e.attr);
+        fuse_reply_entry(req, &e);
+    }
 }
+struct dirbuf {
+    char *p;
+    size_t size;
+};
+static void dirbuf_add(fuse_req_t req, struct dirbuf *b, const char *name,
+                       fuse_ino_t ino)
+{
+    struct stat stbuf;
+    size_t oldsize = b->size;
+    b->size += fuse_add_direntry(req, NULL, 0, name, NULL, 0);
+    b->p = (char *) realloc(b->p, b->size);
+    memset(&stbuf, 0, sizeof(stbuf));
+    stbuf.st_ino = ino;
+    fuse_add_direntry(req, b->p + oldsize, b->size - oldsize, name, &stbuf,
+                      b->size);
+}
+#define min(x, y) ((x) < (y) ? (x) : (y))
+static int reply_buf_limited(fuse_req_t req, const char *buf, size_t bufsize,
+                             off_t off, size_t maxsize)
+{
+    if (off < bufsize)
+        return fuse_reply_buf(req, buf + off,
+                              min(bufsize - off, maxsize));
+    else
+        return fuse_reply_buf(req, NULL, 0);
+}
+static void hello_ll_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
+                             off_t off, struct fuse_file_info *fi)
+{
+    (void) fi;
+    if (ino != 1)
+        fuse_reply_err(req, ENOTDIR);
+    else {
+        struct dirbuf b;
+        memset(&b, 0, sizeof(b));
+        dirbuf_add(req, &b, ".", 1);
+        dirbuf_add(req, &b, "..", 1);
+        dirbuf_add(req, &b, hello_name, 2);
+        reply_buf_limited(req, b.p, b.size, off, size);
+        free(b.p);
+    }
+}
+static void hello_ll_open(fuse_req_t req, fuse_ino_t ino,
+                          struct fuse_file_info *fi)
+{
+    if (ino != 2)
+        fuse_reply_err(req, EISDIR);
+    else if ((fi->flags & O_ACCMODE) != O_RDONLY)
+        fuse_reply_err(req, EACCES);
+    else
+        fuse_reply_open(req, fi);
+}
+static void hello_ll_read(fuse_req_t req, fuse_ino_t ino, size_t size,
+                          off_t off, struct fuse_file_info *fi)
+{
+    (void) fi;
+    assert(ino == 2);
+    reply_buf_limited(req, hello_str, strlen(hello_str), off, size);
+}
+static const struct fuse_lowlevel_ops hello_ll_oper = {
+        .lookup         = hello_ll_lookup,
+        .getattr        = hello_ll_getattr,
+        .readdir        = hello_ll_readdir,
+        .open           = hello_ll_open,
+        .read           = hello_ll_read,
+};
 int main(int argc, char *argv[])
 {
-    int ret;
     struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-    /* Set defaults -- we have to use strdup so that
-       fuse_opt_parse can free the defaults if other
-       values are specified */
-    options.filename = strdup("hello");
-    options.contents = strdup("Hello World!\n");
-    /* Parse options */
-    if (fuse_opt_parse(&args, &options, option_spec, NULL) == -1)
+    struct fuse_session *se;
+    struct fuse_cmdline_opts opts;
+    int ret = -1;
+    if (fuse_parse_cmdline(&args, &opts) != 0)
         return 1;
-    /* When --help is specified, first print our own file-system
-       specific help text, then signal fuse_main to show
-       additional help (by adding `--help` to the options again)
-       without usage: line (by setting argv[0] to the empty
-       string) */
-    if (options.show_help) {
-        show_help(argv[0]);
-        assert(fuse_opt_add_arg(&args, "--help") == 0);
-        args.argv[0][0] = '\0';
+    if (opts.show_help) {
+        printf("usage: %s [options] <mountpoint>\n\n", argv[0]);
+        fuse_cmdline_help();
+        fuse_lowlevel_help();
+        ret = 0;
+        goto err_out1;
+    } else if (opts.show_version) {
+        printf("FUSE library version %s\n", fuse_pkgversion());
+        fuse_lowlevel_version();
+        ret = 0;
+        goto err_out1;
     }
-    ret = fuse_main(args.argc, args.argv, &hello_oper, NULL);
+    if(opts.mountpoint == NULL) {
+        printf("usage: %s [options] <mountpoint>\n", argv[0]);
+        printf("       %s --help\n", argv[0]);
+        ret = 1;
+        goto err_out1;
+    }
+    se = fuse_session_new(&args, &hello_ll_oper,
+                          sizeof(hello_ll_oper), NULL);
+    if (se == NULL)
+        goto err_out1;
+    if (fuse_set_signal_handlers(se) != 0)
+        goto err_out2;
+    if (fuse_session_mount(se, opts.mountpoint) != 0)
+        goto err_out3;
+    fuse_daemonize(opts.foreground);
+    /* Block until ctrl+c or fusermount -u */
+    if (opts.singlethread)
+        ret = fuse_session_loop(se);
+    else
+        ret = fuse_session_loop_mt(se, opts.clone_fd);
+    fuse_session_unmount(se);
+    err_out3:
+    fuse_remove_signal_handlers(se);
+    err_out2:
+    fuse_session_destroy(se);
+    err_out1:
+    free(opts.mountpoint);
     fuse_opt_free_args(&args);
-    return ret;
-}
+    return ret ? 1 : 0;
